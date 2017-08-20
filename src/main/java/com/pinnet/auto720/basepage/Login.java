180fe230237dad24dead593953e6b602eb7c1ae4
xnningnanjing <xnningnanjing@163.com>
2017-08-20 10:17:12 +0800

    add sout

--- src/main/java/com/pinnet/auto720/basepage/Login.java
+++ src/main/java/com/pinnet/auto720/basepage/Login.java
@@ -1,12 +1,10 @@
 package com.pinnet.auto720.basepage;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
 /**
  * Created by nnj on 2016/12/25.
+ *
  */
+
 public class Login {
 
     public void login(){
